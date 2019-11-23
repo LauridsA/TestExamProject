bcb77e9c757aaa8d776df73d5deb20ca4ec2a49c
Laurids Andersen <Laurids.Andersen@jypo.rootdom.dk>
2019-11-23 16:17:36 +0100

    added general structure

---
+++ DataAccess/ADO/BookingRepo.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.ADO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.ADO
+{
+    public class BookingRepo : IBookingRepo
+    {
+
+    }
+}
---
+++ Services/BookingService.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.ADO;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class BookingService : IBookingService
+    {
+        public BookingService()
+        {
+            BookingRepo da = new BookingRepo();
+        }
+    }
+}
