9b6f9dca9e8eb2e69915791bc7f92e16c73984df
LauridsA <Lauridsandersen2013@gmail.com>
2020-01-05 00:18:35 +0100

    more structure

---
+++ Testing/UT/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            var senselessVariable = 1;
+
+            //act
+            var res = senselessVariable + 1;
+
+            //assert
+            Assert.AreEqual(res, senselessVariable + 1);
+        }
+    }
+}
